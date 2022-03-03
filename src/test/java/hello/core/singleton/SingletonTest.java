1250bc475f21e597f93d9f01ec56c9b347b854f2
jisu <jisuuoo96@gmail.com>
2022-03-03 12:06:51 +0900

    [test] : Add a test using a spring container

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -13,8 +15,8 @@ public class SingletonTest {
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
     void pureContainer() {
-        AppConfig appConfig = new AppConfig();
         // 1. 조회: 호출할 때 마다 객체를 생성
+        AppConfig appConfig = new AppConfig();
         MemberService memberService1 = appConfig.memberService();
 
         // 2. 조회: 호출할 때 마다 객체를 생성
@@ -40,5 +42,20 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
         //same ==
         //equal 자바에서 equals라는 메소드를 오버라이드 할 수 있는데 그걸 비교한 것
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        //AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
