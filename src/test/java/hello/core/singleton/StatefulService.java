477c7b18f1eca6895b1c8d5fd80024c1431965c0
jisu <jisuuoo96@gmail.com>
2022-03-04 13:01:44 +0900

    [test] : Additional problem solving that occurs when the status is maintained

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -4,14 +4,15 @@ public class StatefulService {
 
     private int price; // 상태를 유지하는 필드 10000 -> 20000
 
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
         this.price = price; // 여기가 문제!
-    }
-
-    public int getPrice() {
         return price;
     }
+
+//    public int getPrice() {
+//        return price;
+//    }
 
 
 }
