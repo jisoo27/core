a03ffb81663fdc8f4f1d95fe23ece48629e7b7fd
jisu <jisuuoo96@gmail.com>
2022-03-06 15:10:52 +0900

    [feat] : 필드 주입 - 필드에 바로 주입하는 방법 - 코드가 간결하지만 외부 변경이 불가능해서 테스트 하기 힘들다는 단점이 존재함 - DI 프레임워크가 없으면 아무것도 할 수 없음 - 스프링 설정을 목적으로 하는 @Configuration 같은 곳에서만 특별한 용도로 사용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,21 +4,14 @@ import hello.core.discount.DiscountPolicy;
 
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
