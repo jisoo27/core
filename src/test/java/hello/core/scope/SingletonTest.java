290891137acb576d447efbdb3e3bfbeb09ad025e
jisu <jisuuoo96@gmail.com>
2022-03-08 12:44:39 +0900

    [feat] : 싱글톤 스코프 빈 테스트하기 - 빈 초기화 메서드를 실행하고 - 같은 인스턴스의 빈을 조회하고 - 종료 메서드까지 정상 호출 된 것을 확인할 수 있음

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,43 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("SingletonBean1 = " + singletonBean1);
+        System.out.println("SingletonBean2 = " + singletonBean2);
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        ac.close();
+    }
+
+    // 싱글톤 생성정보가 있는 클래스
+    @Scope("singleton")
+    static class SingletonBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+
+    }
+}
