c1993273de69b19687d5967a79d1073f779b0c2b
jisu <jisuuoo96@gmail.com>
2022-03-08 14:09:50 +0900

    [feat] : 싱글톤 빈과 프로토타입 빈을 함께 사용시 항상 새로운 프로토타입 빈 생성하기 - ObjectProvider 사용

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,7 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -25,7 +26,7 @@ public class SingletonWithPrototypeTest1 {
     }
 
     @Test
-    void singletonClientUsePrototype() {
+    void  singletonClientUsePrototype() {
         AnnotationConfigApplicationContext ac =
                 new AnnotationConfigApplicationContext(PrototypeBean.class, ClientBean.class);
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
@@ -34,19 +35,17 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성시점에 주입
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanObjectProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanObjectProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
