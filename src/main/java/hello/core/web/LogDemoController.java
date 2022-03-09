481cff25a9e6d46fd0a1e1ea94cd08c9cca7f919
jisu <jisuuoo96@gmail.com>
2022-03-09 13:24:16 +0900

    [feat] : request 스코프 예제 개발하기 - 동시에 여러 HTTP 요청이 오면 정확히 어떤 요청이 남긴 로그인지 구분하기가 어려운데 'request 스코프'를 사용하면 쉽게 알 수 있다. - MyLogger : 로그를 출력하는 클래스 - LogDemoController : 로거가 잘 작동하는지 확인하는 테스트용 컨트롤러 - LogDemoService : 비지니스 로직이 있는 클래스 - 실행시 오류 발생 (request 스코프 빈은 실제 고객의 요청이 와야 생성됨)

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,29 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody// 뷰 화면 없이 바로 문자를 반환하고 싶다
+    public String logDemo(HttpServletRequest request) { // 요청을 받을 수 있다.
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "ok";
+    }
+
+}
